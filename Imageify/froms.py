c3b9196d0c5fa31d84096930c732d4a3b0d18847
Bidyut Maji <bidyutmaji12345@gmail.com>
2022-01-17 21:15:25 +0530

    RK+GN+APC+JPS

--- Imageify/froms.py
+++ Imageify/froms.py
@@ -2,4 +2,10 @@ from django import forms
 from django import forms
 
 class ImageSearch(forms.Form):
-    search = forms.CharField(max_length='50')
+    search = forms.CharField(max_length=50)
+    num_image = forms.IntegerField(max_value=25, required=False)
+    quality = forms.ChoiceField(choices=(
+        ('raw', '4k'),
+        ('full', 'Full HD'),
+        ('regular', "HD")
+    ))
